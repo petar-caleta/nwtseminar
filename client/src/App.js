93e03e9d7599e658aa381ee01357df77661af231
petar-caleta <pcalet00@fesb.hr>
2020-09-18 03:53:05 +0200

    Some frontend profile

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
+import CreateProfile from './components/profile-forms/CreateProfile';
+import EditProfile from './components/profile-forms/EditProfile';
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
@@ -12,6 +14,7 @@ import { loadUser } from './actions/auth';
 import  setAuthToken  from './utils/setAuthToken';
 import Dashboard from './components/dashboard/Dashboard';
 import PrivateRoute from './components/routing/PrivateRoute';
+
 
 import './App.css';
 
@@ -36,6 +39,8 @@ const App = () => {
             <Route exact path='/register' component = {Register}  />
             <Route exact path='/login' component = {Login}  />
             <PrivateRoute exact path='/dashboard' component = {Dashboard}  />
+            <PrivateRoute exact path='/create-profile' component = {CreateProfile}  />
+            <PrivateRoute exact path='/edit-profile' component = {EditProfile}  />
           </Switch>
         </section>
       </Fragment>
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -4,16 +4,24 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { logout } from '../../actions/auth';
 
+
+
 export const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const authLinks = (
     <ul>
-        <li>
-          <a onClick={logout} href='#!'>
-          <i className="fas fa-sign-out-alt"></i>{' '}
-          <span className="hide-sm">Logout</span>
-          </a>
-        </li>
-      </ul>
+      <li>
+        <Link to='/dashboard'>
+        <i className="fas fa-user"></i>{' '}
+        <span className="hide-sm">Dashboard</span>
+        </Link>
+      </li>
+      <li>
+        <a onClick={logout} href='#!'>
+        <i className="fas fa-sign-out-alt"></i>{' '}
+        <span className="hide-sm">Logout</span>
+        </a>
+      </li>
+    </ul>
   );
 
   const guestLinks = (
